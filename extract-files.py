5ff0499747defa9cc76e0d30bcf28ab5217d113f
Han Sol Jin <hansol@hansol.ca>
2025-01-03 06:39:44 -0800

    judyp: switch to py extract-utils
    Change-Id: I40b9f58615c821adf81060caf027cf26aa723d54

---
+++ extract-files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "hardware/lge",
+    "vendor/lge/sdm845-common",
+    "vendor/qcom/opensource/display",
+]
+
+module = ExtractUtilsModule(
+    'judyp',
+    'lge',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sdm845-common', module.vendor)
+    utils.run()
